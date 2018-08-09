af73e6e4864824783e9c7753e58c35a01cad2f26
Karan Bhandari <karan7752@gmail.com>
2018-08-09 13:14:26 -0400

    rearranginf the already existing files

---
+++ datastructures/linkedlist.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+class Node {
+	int data;
+	Node *next;
+
+	Node(int a): data(a), next(null) {}
+};
+
+
+int main(int argc, char* args[]){
+
+	
+
+
+	for(int i = 1; i < argc; i++) {
+		printf("%s\n", args[i]);
+	}
+
+	return 0;
+}
+
