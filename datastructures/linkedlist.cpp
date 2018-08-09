dfae90be14b6043dd94cc98eb23b984ef5a3c72e
Karan Bhandari <karan7752@gmail.com>
2018-08-09 13:20:04 -0400

    added gitignore file

--- datastructures/linkedlist.cpp
+++ datastructures/linkedlist.cpp
@@ -9,14 +9,9 @@ class Node {
 
 
 int main(int argc, char* args[]){
-
-	
-
-
 	for(int i = 1; i < argc; i++) {
 		printf("%s\n", args[i]);
 	}
-
 	return 0;
 }
 
--- datastructures/stack.cpp
+++ datastructures/stack.cpp
@@ -1,1 +1,11 @@
 // implementation of a stack adt
+#include <vector>
+using namespace std;
+
+class Stack {
+	vector<int> stack;
+};
+
+int main() {
+	// add stack usecases here
+}
