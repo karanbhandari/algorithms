666489133c93350c08d327f754bc2d4afeef52d2
Karan Bhandari <karan7752@gmail.com>
2018-10-03 23:39:29 -0400

    added reverseSrting leetcode challenge

---
+++ algorithms/sorting/mergeSort.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+void mergeSort() {
+	
+}
+
+int main() {
+	int list[5] = {6,2,4,1,3};
+
+	// algorithm
+
+
+	for (int i = 0; i < 5; i++) {
+		cout << list[i] << endl;
+	}
+}
